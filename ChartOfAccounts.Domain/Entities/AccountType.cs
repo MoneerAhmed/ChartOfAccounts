1dbfbb5950f0083eaac018af5c9c3fb72c12554b
MoneerAhmed <moneerahmedfottene@gmail.com>
2026-08-14 19:19:31 +0300

    xAdd project files.

---
+++ ChartOfAccounts.Domain/Entities/AccountType.cs
@@ -0,0 +1,12 @@
+﻿namespace ChartOfAccounts.Domain.Entities;
+
+public class AccountType
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+}
+
+
---
+++ ChartOfAccounts.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,64 @@
+﻿using ChartOfAccounts.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChartOfAccounts.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<AccountType> AccountTypes { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Account>()
+      .HasKey(x => x.Id);
+
+        modelBuilder.Entity<Account>()
+    .Property(x => x.Code)
+    .IsRequired();
+        modelBuilder.Entity<Account>()
+    .Property(x => x.Code)
+    .HasMaxLength(50);
+        modelBuilder.Entity<Account>()
+    .Property(x => x.Name)
+    .IsRequired();
+        modelBuilder.Entity<Account>()
+    .Property(x => x.Name)
+    .IsRequired();
+
+        modelBuilder.Entity<Account>()
+            .Property(x => x.Name)
+            .HasMaxLength(200);
+
+
+        modelBuilder.Entity<Account>()
+    .HasOne(x => x.AccountType)
+    .WithMany()
+    .HasForeignKey(x => x.AccountTypeId);
+
+
+        modelBuilder.Entity<Account>()
+    .HasOne(x => x.Parent)
+    .WithMany(x => x.Children)
+    .HasForeignKey(x => x.ParentId)
+    .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Account>()
+    .Property(x => x.IsActive)
+    .HasDefaultValue(true);
+        modelBuilder.Entity<Account>()
+    .Property(x => x.Nature)
+    .IsRequired();
+
+        modelBuilder.Entity<Account>()
+    .Property(x => x.IsPosting)
+    .HasDefaultValue(false);
+    }
+}
